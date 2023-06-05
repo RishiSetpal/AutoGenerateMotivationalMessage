045868789d550f75e48af361887c10563704f10e
RishiSetpal <rishisetpal123@gmail.com>
2023-06-05 13:00:29 +0530

    Initial Commit
    An Application to Auto-generate random Motivational Messages using fetch API [using HTML, CSS, and JavaScript]

---
+++ javascript1.js
@@ -0,0 +1,12 @@
+let generateMsg = () => {
+    let message = document.getElementById("msg");
+
+    let urlAddress = "http://api.quotable.io/random";
+    
+    fetch(urlAddress)
+    .then((response) => response.json())
+    .then((data) => message.innerHTML = data.content +" by "+data.author)
+    .catch((error) => message.innerHTML = error.message)
+
+
+}
